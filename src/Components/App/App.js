362f21508997bf7879ca878a396b63aa9159c81a
Marcus Svensson <126871549+marcussvenssons@users.noreply.github.com>
2023-04-18 10:29:06 +0200

    Changed structur of files with folders

---
+++ src/Components/App/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Spotify Playlist Project</h1>
+      <button>Save to Spotify</button>
+      <button>Search</button>
+    </div>
+  );
+}
+
+export default App;
